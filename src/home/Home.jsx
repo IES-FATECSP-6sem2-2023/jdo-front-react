e2e1b60dadbcb162150c828998e15e6f70ec778f
Lucas Lima <lima1301lucas@gmail.com>
2023-09-24 15:59:02 -0300

    Página home completa

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -29,6 +29,26 @@ function Home(){
                             </div>
                         </div>
                     </div>
+                    <div className="menu-itens-config">
+                        <div className="item-config">
+                            <div className="item-icon icon-som"></div>
+                        </div>
+                        <div className="item-config">
+                            <div className="item-icon icon-conta"></div>
+                        </div>
+                        <div className="item-config">
+                            <div className="item-icon icon-logout"></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="main">
+                    <div className="arena"></div>
+                    <div className="menu-principal">
+                        <div className="menu-item"><p className="texto-p">JOGAR <br></br> COMO ONÇA</p></div>
+                        <div className="menu-item"><p className="texto-p">JOGAR COMO <br></br> CACHORRO</p></div>
+                        <div className="menu-item"><p className="texto-p">LOJA</p></div>
+                        <div className="menu-item"><p className="texto-p">COLEÇÃO</p></div>
+                    </div>
                 </div>
             </div>
         </section>
