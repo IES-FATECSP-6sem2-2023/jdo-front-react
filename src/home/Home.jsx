09653f43ca7065f9af59f2a25f8ab53261f6b52e
Lucas Lima <lima1301lucas@gmail.com>
2023-09-24 12:45:44 -0300

    Itens do jogador do emnu superior prontos

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -3,7 +3,35 @@ import './Home.css'
 
 function Home(){
     return (
-        <h1>Teste</h1>
+        <section className="bg-home">
+            <div className="bg-home-container">
+                <div className="menu-superior">
+                    <div className="menu-infos-user">
+                        <div className="item-menu nivel">
+                            <div className="icon icon-nivel">
+                                <p className="numNivel">1</p>
+                            </div>
+                            <div className="info inivel">
+                                <p className="info-p">100</p>
+                                <p className="info-p">/100</p>
+                            </div>
+                        </div>
+                        <div className="item-menu esmeraldas">
+                            <div className="icon icon-esmeralda"></div>
+                            <div className="info-esmeralda">
+                                <p className="info-p">1000</p>
+                            </div>
+                        </div>
+                        <div className="item-menu moedas">
+                        <div className="icon icon-moeda"></div>
+                            <div className="info-esmeralda">
+                                <p className="info-p">1000</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
