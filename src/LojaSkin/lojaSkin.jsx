96d223011acc1023b7ff14abe6225978a749704a
Lucas Lima <lima1301lucas@gmail.com>
2023-09-27 11:00:38 -0300

    Loja de skins prontas

--- src/LojaSkin/lojaSkin.jsx
+++ src/LojaSkin/lojaSkin.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './lojaSkin.css'
+import Skin from '../assets/imagens/skins/onca_amazonia.png'
 
 function lojaSkin(){
     return(
@@ -17,7 +18,152 @@ function lojaSkin(){
                         </div>
                     </div>
                 </div>
-                <div className="loja"></div>
+                <div className="loja">
+                    <div className="loja-item">
+                        <div className="skin">
+                            <img src={Skin} alt="" />
+                        </div>
+                        <div className="nome-skin">
+                            <h1>NOME DA SKIN</h1>
+                        </div>
+                        <div className="precos">
+                            <div className="preco-esmeralda">
+                                <div className="icon icon-esmeralda"></div>
+                                <div className="preco"><p>100</p></div>
+                            </div>
+                            <div className="preco-moeda">
+                                <div className="icon icon-moeda"></div>
+                                <div className="preco"><p>1000</p></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="loja-item">
+                        <div className="skin">
+                            <img src={Skin} alt="" />
+                        </div>
+                        <div className="nome-skin">
+                            <h1>NOME DA SKIN</h1>
+                        </div>
+                        <div className="precos">
+                            <div className="preco-esmeralda">
+                                <div className="icon icon-esmeralda"></div>
+                                <div className="preco"><p>100</p></div>
+                            </div>
+                            <div className="preco-moeda">
+                                <div className="icon icon-moeda"></div>
+                                <div className="preco"><p>1000</p></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="loja-item">
+                        <div className="skin">
+                            <img src={Skin} alt="" />
+                        </div>
+                        <div className="nome-skin">
+                            <h1>NOME DA SKIN</h1>
+                        </div>
+                        <div className="precos">
+                            <div className="preco-esmeralda">
+                                <div className="icon icon-esmeralda"></div>
+                                <div className="preco"><p>100</p></div>
+                            </div>
+                            <div className="preco-moeda">
+                                <div className="icon icon-moeda"></div>
+                                <div className="preco"><p>1000</p></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="loja-item">
+                        <div className="skin">
+                            <img src={Skin} alt="" />
+                        </div>
+                        <div className="nome-skin">
+                            <h1>NOME DA SKIN</h1>
+                        </div>
+                        <div className="precos">
+                            <div className="preco-esmeralda">
+                                <div className="icon icon-esmeralda"></div>
+                                <div className="preco"><p>100</p></div>
+                            </div>
+                            <div className="preco-moeda">
+                                <div className="icon icon-moeda"></div>
+                                <div className="preco"><p>1000</p></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="loja-item">
+                        <div className="skin">
+                            <img src={Skin} alt="" />
+                        </div>
+                        <div className="nome-skin">
+                            <h1>NOME DA SKIN</h1>
+                        </div>
+                        <div className="precos">
+                            <div className="preco-esmeralda">
+                                <div className="icon icon-esmeralda"></div>
+                                <div className="preco"><p>100</p></div>
+                            </div>
+                            <div className="preco-moeda">
+                                <div className="icon icon-moeda"></div>
+                                <div className="preco"><p>1000</p></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="loja-item">
+                        <div className="skin">
+                            <img src={Skin} alt="" />
+                        </div>
+                        <div className="nome-skin">
+                            <h1>NOME DA SKIN</h1>
+                        </div>
+                        <div className="precos">
+                            <div className="preco-esmeralda">
+                                <div className="icon icon-esmeralda"></div>
+                                <div className="preco"><p>100</p></div>
+                            </div>
+                            <div className="preco-moeda">
+                                <div className="icon icon-moeda"></div>
+                                <div className="preco"><p>1000</p></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="loja-item">
+                        <div className="skin">
+                            <img src={Skin} alt="" />
+                        </div>
+                        <div className="nome-skin">
+                            <h1>NOME DA SKIN</h1>
+                        </div>
+                        <div className="precos">
+                            <div className="preco-esmeralda">
+                                <div className="icon icon-esmeralda"></div>
+                                <div className="preco"><p>100</p></div>
+                            </div>
+                            <div className="preco-moeda">
+                                <div className="icon icon-moeda"></div>
+                                <div className="preco"><p>1000</p></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="loja-item">
+                        <div className="skin">
+                            <img src={Skin} alt="" />
+                        </div>
+                        <div className="nome-skin">
+                            <h1>NOME DA SKIN</h1>
+                        </div>
+                        <div className="precos">
+                            <div className="preco-esmeralda">
+                                <div className="icon icon-esmeralda"></div>
+                                <div className="preco"><p>100</p></div>
+                            </div>
+                            <div className="preco-moeda">
+                                <div className="icon icon-moeda"></div>
+                                <div className="preco"><p>1000</p></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
