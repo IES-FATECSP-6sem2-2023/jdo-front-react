03a3399535027d8d63b75369a5117db7a756b55c
Lucas Lima <lima1301lucas@gmail.com>
2023-09-27 11:20:53 -0300

    Estrutura loja de moedas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 //import Home from './home/home.jsx'
-import LojaSkin from './lojaSkin/lojaSkin.jsx'
+//import LojaSkin from './lojaSkin/lojaSkin.jsx'
+import LojaMoedas from './lojaMoedas/lojaMoeda.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     //<Home/>
-    <LojaSkin/>
+    //<LojaSkin/>
+    <LojaMoedas/>
   )
 }
 
---
+++ src/LojaMoedas/lojaMoeda.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './lojaMoeda.css'
+//import Skin from '../assets/imagens/skins/onca_amazonia.png'
+
+function lojaMoedas(){
+    return(
+        <section className="bg-home">
+            <div className="bg-home-container">
+                <div className="menu-superior">
+                    <div className="menu">
+                        <div className="item-retorno">
+                            <div className="icon-retorno"></div>
+                        </div>
+                    </div>
+                    <div className="titulo">
+                        <div className="placa-titulo">
+                            <h1>LOJA</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default lojaMoedas
