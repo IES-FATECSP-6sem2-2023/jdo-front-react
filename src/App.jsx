e739d665ed86e3ce70f4dcdb47797db786edf9fc
Lucas Lima <lima1301lucas@gmail.com>
2023-10-08 14:14:59 -0300

    Atualização - Tabuleiro, emblemas e home

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { useState } from 'react'
-//import Home from './Home/home.jsx'
+import Home from './Home/home.jsx'
 //import LojaSkin from './LojaSkin/lojaSkin.jsx'
 //import LojaMoedas from './LojaMoedas/lojaMoeda.jsx'
-import Colecao from './Colecao/colecao.jsx'
+//import Colecao from './Colecao/colecao.jsx'
+// import Conta from './Conta/conta.jsx'
+// import Tabuleiro from './Tabuleiro/tabuleiro.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    //<Home/>
+    <Home/>
     //<LojaSkin/>
     //<LojaMoedas/>
-    <Colecao/>
+    //<Colecao/>
+    //<Conta/>
+    // <Tabuleiro/>
   )
 }
 
--- src/Tabuleiro/tabuleiro.jsx
+++ src/Tabuleiro/tabuleiro.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './tabuleiro.css'
+
 import placaUser from '../assets/imagens/placas/placa_usuario.png'
 
 function Tabuleiro(){
@@ -8,6 +8,15 @@ function Tabuleiro(){
             <div className="bg-home-container">
                 <div className="area-onca">
                     <div className="area-onca-container">
+                        <div className="placar">
+                            <div className="placar-onca"></div> 
+                            {/* Add classe "peca-comida" para mostrar o placar */}
+                            <div className="placar-onca"></div>
+                            <div className="placar-onca"></div>
+                            <div className="placar-onca"></div>
+                            <div className="placar-onca"></div>
+                            <div className="placar-onca"></div>
+                        </div>
                         <div className="contador">
                             <h1>10</h1>
                         </div>
@@ -20,54 +29,56 @@ function Tabuleiro(){
                 <div className="principal">
                     <div className="tabuleiro">
                         <div className="tabuleiro-container">
-                            <div className="row">
-                                <div className="cell" data-x="0" data-y="7"></div>
-                                <div className="cell" data-x="1" data-y="7"></div>
-                                <div className="cell" data-x="3" data-y="7"></div>
-                                <div className="cell" data-x="4" data-y="7"></div>
-                                <div className="cell" data-x="5" data-y="7"></div>
-                            </div>
-                            <div className="row">
-                                <div className="cell" data-x="0" data-y="7"></div>
-                                <div className="cell" data-x="1" data-y="7"></div>
-                                <div className="cell" data-x="3" data-y="7"></div>
-                                <div className="cell" data-x="4" data-y="7"></div>
-                                <div className="cell" data-x="5" data-y="7"></div>
-                            </div>
-                            <div className="row">
-                                <div className="cell" data-x="0" data-y="7"></div>
-                                <div className="cell" data-x="1" data-y="7"></div>
-                                <div className="cell" data-x="3" data-y="7"></div>
-                                <div className="cell" data-x="4" data-y="7"></div>
-                                <div className="cell" data-x="5" data-y="7"></div>
-                            </div>
-                            <div className="row">
-                                <div className="cell" data-x="0" data-y="7"></div>
-                                <div className="cell" data-x="1" data-y="7"></div>
-                                <div className="cell" data-x="3" data-y="7"></div>
-                                <div className="cell" data-x="4" data-y="7"></div>
-                                <div className="cell" data-x="5" data-y="7"></div>
-                            </div>
-                            <div className="row">
-                                <div className="cell" data-x="0" data-y="7"></div>
-                                <div className="cell" data-x="1" data-y="7"></div>
-                                <div className="cell" data-x="3" data-y="7"></div>
-                                <div className="cell" data-x="4" data-y="7"></div>
-                                <div className="cell" data-x="5" data-y="7"></div>
-                            </div>
-                            <div className="row">
-                                <div className="cell" data-x="0" data-y="7"></div>
-                                <div className="cell" data-x="1" data-y="7"></div>
-                                <div className="cell" data-x="3" data-y="7"></div>
-                                <div className="cell" data-x="4" data-y="7"></div>
-                                <div className="cell" data-x="5" data-y="7"></div>
-                            </div>
-                            <div className="row">
-                                <div className="cell" data-x="0" data-y="7"></div>
-                                <div className="cell" data-x="1" data-y="7"></div>
-                                <div className="cell" data-x="3" data-y="7"></div>
-                                <div className="cell" data-x="4" data-y="7"></div>
-                                <div className="cell" data-x="5" data-y="7"></div>
+                            <div className="malha-tabuleiro">
+                                <div class="row">
+                                    <div class="cell peca-cachorro" data-x="0" data-y="0"></div>
+                                    <div class="cell peca-cachorro" data-x="1" data-y="0"></div>
+                                    <div class="cell peca-cachorro" data-x="2" data-y="0"></div>
+                                    <div class="cell peca-cachorro" data-x="3" data-y="0"></div>
+                                    <div class="cell peca-cachorro" data-x="4" data-y="0"></div>
+                                </div>
+                                <div class="row">
+                                    <div class="cell peca-cachorro" data-x="0" data-y="1"></div>
+                                    <div class="cell peca-cachorro" data-x="1" data-y="1"></div>
+                                    <div class="cell peca-cachorro" data-x="2" data-y="1"></div>
+                                    <div class="cell peca-cachorro" data-x="3" data-y="1"></div>
+                                    <div class="cell peca-cachorro" data-x="4" data-y="1"></div>
+                                </div>
+                                <div class="row">
+                                    <div class="cell peca-cachorro" data-x="0" data-y="2"></div>
+                                    <div class="cell peca-cachorro" data-x="1" data-y="2"></div>
+                                    <div class="cell peca-onca" data-x="2" data-y="2"></div>
+                                    <div class="cell peca-cachorro" data-x="3" data-y="2"></div>
+                                    <div class="cell peca-cachorro" data-x="4" data-y="2"></div>
+                                </div>
+                                <div class="row">
+                                    <div class="cell" data-x="0" data-y="3"></div>
+                                    <div class="cell" data-x="1" data-y="3"></div>
+                                    <div class="cell" data-x="2" data-y="3"></div>
+                                    <div class="cell" data-x="3" data-y="3"></div>
+                                    <div class="cell" data-x="4" data-y="3"></div>
+                                </div>
+                                <div class="row">
+                                    <div class="cell" data-x="0" data-y="4"></div>
+                                    <div class="cell" data-x="1" data-y="4"></div>
+                                    <div class="cell" data-x="2" data-y="4"></div>
+                                    <div class="cell" data-x="3" data-y="4"></div>
+                                    <div class="cell" data-x="4" data-y="4"></div>
+                                </div>
+                                <div class="row">
+                                    <div class="cell-tri" data-x="0" data-y="5"></div>
+                                    <div class="cell" data-x="1" data-y="5"></div>
+                                    <div class="cell" data-x="2" data-y="5"></div>
+                                    <div class="cell" data-x="3" data-y="5"></div>
+                                    <div class="cell-tri" data-x="4" data-y="5"></div>
+                                </div>
+                                <div class="row">
+                                    <div class="cell" data-x="0" data-y="6"></div>
+                                    <div class="cell-tri" data-x="1" data-y="6"></div>
+                                    <div class="cell" data-x="2" data-y="6"></div>
+                                    <div class="cell-tri" data-x="3" data-y="6"></div>
+                                    <div class="cell" data-x="4" data-y="6"></div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -99,15 +110,15 @@ function Tabuleiro(){
                         </div>
                     </div>
                     <div className="area-cachorro-container">
-                            <div className="contador">
-                                <h1>10</h1>
-                            </div>
-                            <div className="info-user jogador-cachorro">
-                                <img src={placaUser} />
-                                <h1>JOGADOR CACHORRO</h1>
-                            </div>
+                        <div className="contador">
+                            <h1>10</h1>
+                        </div>
+                        <div className="info-user jogador-cachorro">
+                            <img src={placaUser} />
+                            <h1>JOGADOR CACHORRO</h1>
                         </div>
                     </div>
+                </div>
             </div>
         </section>
     )
