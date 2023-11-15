a415adf46fe48eebc7b69b53e96b1375a4e85772
paulo.goncalves <paulomarquesg06@gmail.com>
2023-11-15 17:19:20 -0300

    crt: create context somAmbiente

--- src/providers/GlobalProvider.jsx
+++ src/providers/GlobalProvider.jsx
@@ -1,12 +1,15 @@
 import { AuthContaProvider } from '../contexts/AuthContaContext';
+import { SomAmbienteProvider } from '../contexts/SomAmbienteContext';
 import { TabuleiroProvider } from '../contexts/TabuleiroContext';
 
 const GlobalProvider = ({children}) => {
     return (
         <AuthContaProvider>
-            <TabuleiroProvider>
-                {children}
-            </TabuleiroProvider>
+            <SomAmbienteProvider>
+                <TabuleiroProvider>
+                    {children}
+                </TabuleiroProvider>
+            </SomAmbienteProvider>
         </AuthContaProvider>
     )
 }
