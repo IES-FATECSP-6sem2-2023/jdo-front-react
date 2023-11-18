555a63bfee0890234395765528258746988c094e
paulo.goncalves <paulomarquesg06@gmail.com>
2023-11-18 01:10:38 -0300

    fix: login com sessao e contexto

--- src/providers/GlobalProvider.jsx
+++ src/providers/GlobalProvider.jsx
@@ -1,16 +1,15 @@
-import { AuthContaProvider } from '../contexts/AuthContaContext';
 import { SomAmbienteProvider } from '../contexts/SomAmbienteContext';
 import { TabuleiroProvider } from '../contexts/TabuleiroContext';
 
 const GlobalProvider = ({children}) => {
     return (
-        <AuthContaProvider>
+        
             <SomAmbienteProvider>
                 <TabuleiroProvider>
                     {children}
                 </TabuleiroProvider>
             </SomAmbienteProvider>
-        </AuthContaProvider>
+        
     )
 }
 
