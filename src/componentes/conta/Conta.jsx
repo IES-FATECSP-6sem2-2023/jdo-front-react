042e6f3c934fc34825c2754aeb5d52e22090d00a
paulo.goncalves <paulomarquesg06@gmail.com>
2023-11-20 12:48:57 -0300

    fix: senha aparece na conta

--- src/componentes/conta/Conta.jsx
+++ src/componentes/conta/Conta.jsx
@@ -84,7 +84,7 @@ function Conta() {
                             </div>
                             <div className="info-conta info-senha-conta">
                                 <label htmlFor="">SENHA:</label>
-                                <input type={visibilityStatus ? "text" : "password"} className='inp-senha' onChange={(e) => setSenha(e.target.value)} required/>
+                                <input type={visibilityStatus ? "text" : "password"} className='inp-senha' onChange={(e) => setSenha(e.target.value)} autoComplete='off' required/>
                                 <button className="btn-visibility btn-senha-conta" onClick={toggleVisibility}>{visibilityStatus ? <VisibilityIcon /> : <VisibilityOffIcon />}</button>
                             </div>
                             <div className="info-conta atualiza-info-conta">
