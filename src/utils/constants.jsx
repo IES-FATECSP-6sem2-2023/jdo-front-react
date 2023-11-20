a39427195e0c61026e200b2cab5014ad0814b245
paulo.goncalves <paulomarquesg06@gmail.com>
2023-11-20 19:31:04 -0300

    att: novo link back

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,2 +1,3 @@
 // export const API_URL = 'http://localhost:8080';
-export const API_URL = 'https://jogodaoncabackend.onrender.com';
+// export const API_URL = 'https://jogodaoncabackend.onrender.com';
+export const API_URL = 'https://jogodaoncabackend-c856712bb044.herokuapp.com/';
