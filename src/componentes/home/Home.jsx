12d4d8b461ef931ab928cc91e16b9ccd2c090e51
paulo.goncalves <paulomarquesg06@gmail.com>
2023-11-20 20:58:22 -0300

    att: remocao texto teste

--- src/componentes/home/Home.jsx
+++ src/componentes/home/Home.jsx
@@ -77,7 +77,6 @@ function Home() {
                         <Carrossel />
                     </div>
                     <div className="menu-principal">
-                        <p>ISSO É UM TESTE!</p>
                         <button className="btn menu-item" onClick={() => jogar('onca')}><p className="texto-p">JOGAR <br></br> COMO ONÇA</p></button>
                         <button className="btn menu-item" onClick={() => jogar('cachorro')}><p className="texto-p">JOGAR COMO <br></br> CACHORRO</p></button>
                         <button className="btn menu-item" onClick={() => {navigate("/loja/skins")}}><p className="texto-p">LOJA</p></button>
