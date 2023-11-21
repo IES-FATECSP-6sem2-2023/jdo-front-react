a043fea69e8f84272a6bfde6e48ece518e03362f
paulo.goncalves <paulomarquesg06@gmail.com>
2023-11-21 14:24:17 -0300

    fix: const

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:8080';
+// export const API_URL = 'http://localhost:8080';
 // export const API_URL = 'https://jogodaoncabackend.onrender.com';
 // export const API_URL = 'https://jogodaoncabackend-c856712bb044.herokuapp.com/';
-// export const API_URL = 'https://apijogodaonca.azurewebsites.net';
+export const API_URL = 'https://apijogodaonca.azurewebsites.net';
